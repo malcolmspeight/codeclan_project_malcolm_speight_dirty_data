338b8db86ffd2315e41b1695e45cf3c1a8f2ac80
malcolmspeight <115028737+malcolmspeight@users.noreply.github.com>
2022-11-29 16:37:41 +0000

    checks on age

--- task_04/scripts_data_cleaning/candy_cleaning_script.R
+++ task_04/scripts_data_cleaning/candy_cleaning_script.R
@@ -2,15 +2,16 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 
-view(candy_2015)
-glimpse(candy_2015)
-
 ## 2015 data
 # read in data and clean variable names
 candy_2015 <- read_excel("data_raw/boing-boing-candy-2015.xlsx") %>% 
-  mutate(how_old_are_you = as.numeric(how_old_are_you)) # force age to be numeric
   select(1:96, 115) %>% # remove unwanted columns
   clean_names()
+
+# extract year from timestamp column then remove timestamp
+  candy_2015 <- candy_2015 %>% 
+    mutate(year = str_sub(timestamp, start = 1, end = 4), .before = timestamp) %>% 
+    select(-timestamp)
 
 # rename certain columns
 candy_2015 <- candy_2015 %>% 
@@ -21,8 +22,14 @@ candy_2015 <- candy_2015 %>%
          "joy_joy_mit_iodine!" = "joy_joy_mit_iodine", 
          "licorice_yes_black" = "licorice")
 
-# extract year from timestamp column then remove timestamp
+## age
 candy_2015 <- candy_2015 %>% 
-  mutate(year = str_sub(timestamp, start = 1, end = 4), .before = timestamp) %>% 
-  select(-timestamp)
+  mutate(age = as.numeric(age)) %>% # force age to be numeric
+  filter(!is.na(age)) %>% # remove rows with non-numeric age value 
+  filter(age >= 1 & age <= 100) # remove rows where age 
+  
+
+view(candy_2015)
+glimpse(candy_2015)
+
   
