e62e88b5edadcc5026cc9bdecd02ce6d528366f5
malcolmspeight <115028737+malcolmspeight@users.noreply.github.com>
2022-11-29 15:22:54 +0000

    force age to be numeric

--- task_04/scripts_data_cleaning/candy_cleaning_script.R
+++ task_04/scripts_data_cleaning/candy_cleaning_script.R
@@ -8,6 +8,7 @@ glimpse(candy_2015)
 ## 2015 data
 # read in data and clean variable names
 candy_2015 <- read_excel("data_raw/boing-boing-candy-2015.xlsx") %>% 
+  mutate(how_old_are_you = as.numeric(how_old_are_you)) # force age to be numeric
   select(1:96, 115) %>% # remove unwanted columns
   clean_names()
 
@@ -25,5 +26,3 @@ candy_2015 <- candy_2015 %>%
   mutate(year = str_sub(timestamp, start = 1, end = 4), .before = timestamp) %>% 
   select(-timestamp)
   
-
-
