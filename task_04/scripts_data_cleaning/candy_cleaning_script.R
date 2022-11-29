e404cd39934988f6ce1460e363eff4472bdd4afd
malcolmspeight <115028737+malcolmspeight@users.noreply.github.com>
2022-11-29 14:39:59 +0000

    rename 2015 columns

--- task_04/scripts_data_cleaning/candy_cleaning_script.R
+++ task_04/scripts_data_cleaning/candy_cleaning_script.R
@@ -9,8 +9,21 @@ glimpse(candy_2015)
 # read in data and clean variable names
 candy_2015 <- read_excel("data_raw/boing-boing-candy-2015.xlsx") %>% 
   select(1:96, 115) %>% # remove unwanted columns
-  clean_names() 
+  clean_names()
 
+# rename certain columns
+candy_2015 <- candy_2015 %>% 
+  rename("age" = "how_old_are_you",
+         "going_out?" = "are_you_going_actually_going_trick_or_treating_yourself",
+         "bonkers_the_candy" = "bonkers", 
+         "hershey_s_dark_chocolate" = "dark_chocolate_hershey", 
+         "joy_joy_mit_iodine!" = "joy_joy_mit_iodine", 
+         "licorice_yes_black" = "licorice")
 
+# extract year from timestamp column then remove timestamp
+candy_2015 <- candy_2015 %>% 
+  mutate(year = str_sub(timestamp, start = 1, end = 4), .before = timestamp) %>% 
+  select(-timestamp)
+  
 
 
