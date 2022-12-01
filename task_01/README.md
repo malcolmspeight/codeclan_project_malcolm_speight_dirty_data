4021686c939d1cee96ac5c28ecdf2a9eab70eed4
malcolmspeight <115028737+malcolmspeight@users.noreply.github.com>
2022-12-01 16:51:50 +0000

    Task 01

---
+++ task_01/README.md
@@ -0,0 +1,5 @@
+This task relates to athletes competing in the decathlon over two meetings, Decastar and OlympicG.
+
+Their times and distances are recorded, as is their placing in the competition and the number of points they scored overall. There are 41 records in the dataset. 
+
+The data is cleaned and then interrogated to answer questions concerning the performance of the decathletes. 
---
+++ task_01/scripts_data_cleaning/decathlon_clean.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(readr)
+library(janitor)
+
+## Decathlon data
+# read in data
+decathlon_data <- read_rds("data_raw/decathlon.rds") %>% 
+  rownames_to_column("athlete") %>% # flip existing row names to a new 'athlete' column
+  clean_names() # clean variable names
+
+# change variable names, data type of 'competition' and format of athlete name values
+decathlon_data <- decathlon_data %>% 
+  rename("100m" = "x100m", 
+         "400m" = "x400m",
+         "110m_hurdle" = "x110m_hurdle",
+         "1500m" = "x1500m") %>% 
+  mutate(competition = as.character(competition)) %>% 
+  mutate(athlete = str_to_title(athlete))
+
+# write data to clean data folder 
+write_csv(decathlon_data, "data_clean/decathlon_clean")
+
+
+
+
+
